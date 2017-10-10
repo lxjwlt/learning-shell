40e53e7e9eb5e62fca507c81b9c50012b4698a40
lxjwlt <lxjwlt.c@gmail.com>
2017-10-10 22:37:20 +0800

    new quiz for check existence

--- quiz/list.js
+++ quiz/list.js
@@ -2,19 +2,21 @@
 
 const random = require('../lib/random');
 const util = require('../lib/util');
+const path = require('path');
 
 module.exports = function () {
     return [
         {
             title: 'list files or directory',
             data: function () {
+                let dirs = random.randomExistPath().directories;
+
                 return {
-                    includeHiddenFile: random.one(true, false),
-                    paths: random.pick([
-                        random.randomExistPath(),
-                        random.randomExistPath(),
+                    includeHiddenFile: random.one(true, false, null),
+                    includeInfo: random.one(true, false),
+                    paths: random.pick(random.pick(dirs, 2).concat(
                         'current directory'
-                    ], random.chance({
+                    ), random.chance({
                         3: 2,
                         7: 1
                     }))
@@ -27,7 +29,13 @@ module.exports = function () {
                     hiddenTail = '';
                 }
 
-                return `List all files under ${data.paths.join(' and ')} directory ${hiddenTail}`;
+                let infoStr = '';
+
+                if (data.includeInfo) {
+                    infoStr = ' and their details';
+                }
+
+                return `List all files${infoStr} under ${data.paths.join(' and ')} directory ${hiddenTail}`;
             },
             validate: function ({cmd, data, args}) {
                 if (args.words[0] !== 'ls') {
@@ -42,6 +50,10 @@ module.exports = function () {
                     return false;
                 }
 
+                if (data.includeInfo && !args.options.l) {
+                    return false;
+                }
+
                 let cmds = cmd.split(/\s+/g).map(util.nicePath);
 
                 if (data.paths.some((p) => p !== 'current directory' && cmds.indexOf(util.nicePath(p)) < 0)) {
@@ -49,6 +61,35 @@ module.exports = function () {
                 }
 
                 return true;
+            }
+        },
+        {
+            title: 'existence of file or directory',
+            data () {
+                let randomPathMap = random.randomExistPath();
+
+                let file = random.array(randomPathMap.files);
+
+                let pathInfo = path.parse(file);
+
+                return {
+                    realPath: pathInfo.dir,
+                    path: random.array(random.pick(randomPathMap.directories, 2).concat(pathInfo.dir)),
+                    fileName: pathInfo.base
+                };
+            },
+            info (data) {
+                return `is there ${data.fileName} inner ${data.path}?`;
+            },
+            prompt (data) {
+                return {
+                    type: 'confirm',
+                    name: 'confirm',
+                    message: `is there ${data.fileName} inner ${data.path}?`
+                }
+            },
+            validate ({data, prompt}) {
+                return (data.realPath === data.path) === prompt.confirm;
             }
         }
     ];
