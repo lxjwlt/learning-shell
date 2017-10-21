c38f6b3ffe37febcc1d0e883caf771ad568d041f
lxjwlt <lxjwlt.c@gmail.com>
2017-10-21 13:59:21 +0800

    new quiz for chmod command

--- quiz/permission.js
+++ quiz/permission.js
@@ -2,6 +2,7 @@
 
 const random = require('../lib/random');
 const util = require('../lib/util');
+const fs = require('fs-extra');
 
 module.exports = function () {
     return [
@@ -38,6 +39,70 @@ module.exports = function () {
 
                 if (data.type === 'write') {
                     return util.canWrite(data.path) === prompt.confirm;
+                }
+            }
+        },
+        {
+            title: 'Change file mode',
+            data: function () {
+                let tempFile = util.createTempFile('test');
+                let isAdd = random.one(true, false);
+
+                fs.chmodSync(tempFile, isAdd ? 0 : '777');
+
+                return {
+                    file: tempFile,
+                    type: random.pick(['write', 'read', 'execute'], random.int(1, 3)),
+                    add: isAdd,
+                    role: random.pick(['owner', 'group', 'others'], random.int(1, 3))
+                };
+            },
+            info: function (data) {
+                let operator = data.add ? 'adding' : 'removing';
+                let operatorJoiner = data.add ? 'for' : 'from';
+
+                let type = data.type.length < 3 ? data.type.join(' and ') : 'all';
+
+                let role = data.role.length < 3 ? data.role.join(' and ') : 'everyone';
+
+                return `set file mode of "${data.file}" by ${operator} ${type} permission ${operatorJoiner} ${role}`;
+            },
+            validate: function ({data}) {
+
+                let map = {
+                    owner: {
+                        read: fs.constants.S_IRUSR,
+                        write: fs.constants.S_IWUSR,
+                        execute: fs.constants.S_IXUSR
+                    },
+                    group: {
+                        read: fs.constants.S_IRGRP,
+                        write: fs.constants.S_IWGRP,
+                        execute: fs.constants.S_IXGRP
+                    },
+                    others: {
+                        read: fs.constants.S_IROTH,
+                        write: fs.constants.S_IWOTH,
+                        execute: fs.constants.S_IXOTH
+                    }
+                };
+
+                let stats = fs.statSync(data.file);
+
+                for (let role of data.role) {
+                    for (let type of data.type) {
+                        let checkMode = stats.mode | map[role][type];
+                        if ((checkMode === stats.mode) !== data.add) {
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+            },
+            destroy (data) {
+                if (data.file) {
+                    fs.removeSync(data.file);
                 }
             }
         }
